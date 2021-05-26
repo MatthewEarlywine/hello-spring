898307ddd4d3079276c913fbd0f49f6839b57740
Matt <matthewearlywine@gmail.com>
2021-05-26 15:35:31 -0500

    Commit for corrected controller-exercises

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -3,9 +3,9 @@ package org.launchcode.hellospring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
 
-@RequestMapping
+@Controller
+@ResponseBody
 public class HelloController {
 
     //Handles requests at path /hello
@@ -17,28 +17,26 @@ public class HelloController {
 
     //responds to GET requests at "/goodbye"
     //now lives at /hello/goodbye
-    @GetMapping ("goodbye")
-    @ResponseBody
+    @GetMapping("goodbye")
     public String goodbye(){
         return "Goodbye, Spring!";
     }
 
     // Handles request of the form /hello?name=LaunchCode
     // lives at /hello/hello
-//    @RequestMapping(method={RequestMethod.GET, RequestMethod.POST})
-//    public String helloWithQueryParam(@RequestParam String name, @RequestParam String value){
-//            return "Hello " + name + "!";
+//    @RequestMapping(method={RequestMethod.GET, RequestMethod.POST}, value = "hello")
+//    @ResponseBody
+//    public String helloWithQueryParam(@RequestParam String name){
+//        return "Hello " + name + "!";
 //    }
 
     // Handles requests of the form /hello/LaunchCode
     @GetMapping ("{name}")
-    @ResponseBody
     public String helloWithPathParam(@PathVariable String name){
         return "Hello " + name +"!";
     }
     // Also know that you can redirect a user by removing the @ResponseBody annotation
     // from the controller and returning "redirect:/(DESIREDPATH)".
-
 
     @RequestMapping(method={RequestMethod.GET, RequestMethod.POST}, value = "hello")
     public String createMessage(@RequestParam String name, @RequestParam String language){
@@ -48,36 +46,49 @@ public class HelloController {
 
         String greeting = "";
 
-        if (language.equals("Fr")) {
+        if (language.equals("french")) {
             greeting = "Bonjour, ";
-        } else if (language.equals("Sp")) {
+        } else if (language.equals("spanish")) {
             greeting = "Hola, ";
-        } else if (language.equals("Ge")) {
+        } else if (language.equals("german")) {
             greeting = "Hallo, ";
-        } else if (language.equals("Ch")) {
+        } else if (language.equals("chinese")) {
             greeting = "Ni hao, ";
-        } else if (language.equals("En")){
+        } else if (language.equals("english")){
             greeting = "Hello ";
         }
         return "<p style='color:blue; text-align:center; margin-top:28px; font-size:38px'>" + greeting + name + "</p>";
     }
 
     // lives at /hello/form
-    @GetMapping ("form")
-    public String helloForm(){
-        return "form";
-    }
-
-//    @RequestMapping(method = RequestMethod.POST)
-//    public String createMessage(@RequestParam String name,@RequestParam String language){
-//        if (name == null){
-//            name = "World";
-//        }
-//
-//        String greeting = "";
-//
-//        if(language)
+//    @GetMapping ("form")
+//    public String helloForm(){
+//        return "<html>" +
+//                "<body>" +
+//                "<form action='hello' method='post'>" + // submits request to /hello
+//                "<input type='text' name='name'>" +
+//                "<input type='submit' value='Greet me!'>" +
+//                "</form?" +
+//                "</body>" +
+//                "</html>";
 //    }
 
 
+        @GetMapping ("form")
+    public String helloForm(){
+        return "<html>" +
+                "<body>" +
+                "<form action='hello' method='post'>" + // submits request to /hello
+                "<input type='text' name='name'>" +
+                "<select name='language'>" +
+                "<option value='english'>English</option>" +
+                "<option value='spanish'>Spanish</option>" +
+                "<option value='german'>German</option>" +
+                "<option value='french'>French</option>" +
+                "<option value='chinese'>Chinese</option>" +
+                "<input type='submit' value='Greet me!'>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
 }
